0aa2c84a1da4edfff3734e4364cf5909fd0827bc
Diego Puentes <djsix2001@yahoo.com>
2024-11-12 18:28:35 -0500

    Updated Structure and workstyle

--- EDO-Project.py
+++ EDO-Project.py
@@ -1,3 +1,34 @@
 import streamlit as st
+import os
 
-st.write("hola")
+st.set_page_config(layout="wide")
+
+st.title("Proyecto de Ecuaciones Diferenciales Ordinarias")
+st.markdown("<hr>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+
+with st.sidebar:
+    st.header("Menu")
+    option = st.radio("Selecciona una opción:", ["Graficadora", "Informe", "Acerca de"])
+
+if option == "Graficadora":
+    
+    st.subheader("Graficadora")
+    
+    interface_path = './UI/interface.py'
+    
+    with open(interface_path, 'r') as file:
+        code = file.read()
+        
+    exec(code)
+    
+    
+    
+elif option == "Informe":
+    st.subheader("Informe")
+    
+elif option == "Acerca de:":
+    st.subheader("Acerca de")
+    
+
+
---
+++ UI/interface.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+col1,col2 = st.columns(2)
+
+col1.write("hola")
