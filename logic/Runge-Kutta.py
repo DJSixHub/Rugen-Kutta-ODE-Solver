2643cee2a7880daaf176ea02c31ef6b6e2f5c056
ALbertEO3 <marichalalberto292@gmail.com>
2024-11-12 21:08:45 -0500

    example

--- logic/Runge-Kutta.py
+++ logic/Runge-Kutta.py
@@ -14,7 +14,7 @@ def runge_kutta(x0:float|int, y0:float|int, h:float, fun:Callable[int|float, int
             k1 = h * f(t[i],y[i],fun)
             k2 = h * f(t[i] + h/2,y[i] + k1/2,fun)
             k3 = h * f(t[i] + h/2,y[i] + k2/2,fun)
-            k4 = h * f(t[i] + h/2,y[i] + k3,fun)
+            k4 = h * f(t[i] + h,y[i] + k3,fun)
             y[i+1] = y[i]+(1/6*k1 + 1/3*k2 + 1/3*k3 + 1/6*k4)
 
         return t,y
@@ -27,11 +27,10 @@ def runge_kutta(x0:float|int, y0:float|int, h:float, fun:Callable[int|float, int
     y0 = y0
     x_span = (x0,10)
     h = h
-    
+
     t,y_r = _runge_kutta(f,y0,x_span,h,fun)
     #hay parametros tengo que ajustarlos para que coja más rango de valores
     sol = solve_ivp(fun, x_span, [y0], t_eval=np.linspace(x0, 10, 100))
- 
     # los dos primeros son los calculados por nosotros, los otros dos son con el de python
     # las t son las x
     # y_r son las y
@@ -43,7 +42,7 @@ def runge_kutta(x0:float|int, y0:float|int, h:float, fun:Callable[int|float, int
 
 
 """
-asi se llama 
+ejemplo de como llamarla
 def f(x,y):
     return x+y
 
