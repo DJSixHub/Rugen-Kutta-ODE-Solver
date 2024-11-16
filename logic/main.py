4b2569d0018301e994ef9f495f08c329294ff6d2
ALbertEO3 <marichalalberto292@gmail.com>
2024-11-15 23:45:44 -0500

    fix runge-kutta

--- logic/main.py
+++ logic/main.py
@@ -4,7 +4,6 @@ from parser import Parser
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 lexer = Lexer(TOKEN_PATTERNS, CONSTANTS)
 tokens: list[Token] = lexer.tokenize(function)
 parser = Parser()
@@ -28,13 +27,14 @@ def RungeKutta(x_point, y_point, h):
 
     #### Runge-Kutta 
     for i in range(len(X)-1): 
-        k1 =  edo(ast,{'x': i, 'y': y[i]})
-        k2 =  edo(ast,{'x': i + h / 2, 'y': y[i] + k1 / 2})
-        k3 =  edo(ast,{'x': i + h / 2, 'y': y[i] + k2 / 2})
-        k4 =  edo(ast,{'x': i + h, 'y': y[i] + k3})
+        k1 =  edo(ast,{'x': X[i], 'y': y[i]})
+        k2 =  edo(ast,{'x': X[i] + h / 2, 'y': y[i] + k1 / 2})
+        k3 =  edo(ast,{'x': X[i] + h / 2, 'y': y[i] + k2 / 2})
+        k4 =  edo(ast,{'x': X[i] + h, 'y': y[i] + k3})
         y[i+1] = (y[i] + h*(1/6 * k1 + 1/3 * k2 + 1/3 * k3 + 1/6 * k4))
    
     return X,y
+
 
 
 RungeKutta(x_point,y_point,h)
