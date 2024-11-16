6c145c75fa0ee1fde47cda29534f6537c5972a24
ALbertEO3 <marichalalberto292@gmail.com>
2024-11-15 23:43:08 -0500

    fix

--- logic/main.py
+++ logic/main.py
@@ -5,7 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 lexer = Lexer(TOKEN_PATTERNS, CONSTANTS)
 tokens: list[Token] = lexer.tokenize(function)
 parser = Parser()
@@ -23,22 +22,22 @@ def edo(ast: Expression, vars: dict):
 
 def RungeKutta(x_point, y_point, h):
     
-    X = np.arange(int(x_point),5,h)
+    X = np.arange(0,5,h)
     y = np.zeros(len(X))
     y[0]=y_point
 
     #### Runge-Kutta 
     for i in range(len(X)-1): 
-        k1 = h * edo(ast,{'x': i, 'y': y[i]})
-        k2 = h * edo(ast,{'x': i + h / 2, 'y': y[i] + k1 / 2})
-        k3 = h * edo(ast,{'x': i + h / 2, 'y': y[i] + k2 / 2})
-        k4 = h * edo(ast,{'x': i + h, 'y': y[i] + k3})
-        y[i+1] = (y[i] + (1/6 * k1 + 1/3 * k2 + 1/3 * k3 + 1/6 * k4))
+        k1 =  edo(ast,{'x': i, 'y': y[i]})
+        k2 =  edo(ast,{'x': i + h / 2, 'y': y[i] + k1 / 2})
+        k3 =  edo(ast,{'x': i + h / 2, 'y': y[i] + k2 / 2})
+        k4 =  edo(ast,{'x': i + h, 'y': y[i] + k3})
+        y[i+1] = (y[i] + h*(1/6 * k1 + 1/3 * k2 + 1/3 * k3 + 1/6 * k4))
    
     return X,y
 
 
-
+RungeKutta(x_point,y_point,h)
 
 
 
